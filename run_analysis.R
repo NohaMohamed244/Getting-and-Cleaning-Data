0dd23e78bfa9d9c124daf742f87c8c90189dcdb3
omershro <40171300+omershro@users.noreply.github.com>
2018-08-16 15:56:08 +0300

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,19 @@
+### downloading and unzipping the data
+
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","week3.zip")
+unzip("week3.zip")
+
+
+### Merge training and test sets to create one data set
+
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+subject <- rbind(subject_test , subject_train)
+
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+x <- rbind(x_test , x_train)
+
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+y <- rbind(y_test , y_train)
