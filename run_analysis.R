ef86ee1166dc98108e5b3e547a787f1d95b158a4
omershro <40171300+omershro@users.noreply.github.com>
2018-08-16 16:00:15 +0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -17,3 +17,27 @@ x <- rbind(x_test , x_train)
 y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
 y <- rbind(y_test , y_train)
+
+
+### Extracts only the measurements on the mean and standard deviation for each measurement
+
+features <- read.table("UCI HAR Dataset/features.txt")
+mean.std.features <- grep("mean|std" , features[,2]) 
+mean.std <- x[ , mean.std.features] 
+
+
+### Uses descriptive activity names to name the activities in the data set
+
+labels <- read.table ("UCI HAR Dataset/activity_labels.txt")
+labels <- as.character(labels[ ,2])
+
+
+### Appropriately labels the data set with descriptive variable names
+
+var_names <- cbind(subject , x , y)
+str(var_names)
+
+
+### From the data set in the previous step, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+tidydata <- aggregate (var_names , by=var_names, FUN = mean)
